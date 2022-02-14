aabc04aab31e51436294158eae692408cfc09737
Юлия Мамшева <swix21012002@gmail.com>
2022-02-14 12:38:17 +0300

    Фокусы для гиперболы

--- InteractivePoster/Finction/DrawHyperbole.cs
+++ InteractivePoster/Finction/DrawHyperbole.cs
@@ -15,7 +15,10 @@ namespace InteractivePoster.Finction
     {
         double rectangleA;
         double rectangleB;
+        double sinGradusElpis;
+        double cosGradusElpis;
         Rectangle rectangle;
+        Canvas cv;
         Line line;
         double x, y,a,b, maxCorrdinatX;
         public DrawHyperbole(double x, double y, double a, double b, Canvas cv, double maxCorrdinatX,double gradusTransform)
@@ -24,12 +27,18 @@ namespace InteractivePoster.Finction
             this.y = y;
             this.a = a;
             this.b = b;
+            this.cv = cv;
+
+            sinGradusElpis = Math.Sin(convertRadian(gradusTransform));
+            cosGradusElpis = Math.Cos(convertRadian(gradusTransform));
+
             this.maxCorrdinatX = maxCorrdinatX;
             count = Convert.ToDouble(cv.Tag);//получаем масштабы области
             maxX = cv.ActualWidth; //получаем ширину канвы       
             rectangleA = a * (maxX / count);//преобразуем ширину прямоугольника из декартовой системы
             rectangleB = b * (maxX / count);//преобразуем высоту прямоугольника из декартовой системы
             DrawRectangle(x, y, cv, gradusTransform);
+            FocusHyperbole();
         }
         QuadraticBezierSegment quadraticBezierSegment;
         public PathGeometry Hyperbola()
@@ -56,6 +65,7 @@ namespace InteractivePoster.Finction
 
             return pathGeometry;
         }
+
         void DrawRectangle(double x, double y, Canvas cv, double gradusTransform)
         {
             rectangle = new Rectangle()
@@ -76,7 +86,47 @@ namespace InteractivePoster.Finction
             rectangle.SetValue(Canvas.LeftProperty, convertX(x));
             rectangle.SetValue(Canvas.TopProperty, convertY(y));
         }
+        void DrawText(double x, double y, string s)
+        {
+            TextBlock TB = new TextBlock();
+            TB.Text = s;
+            cv.Children.Add(TB);
+            TB.SetValue(Canvas.LeftProperty, convertCoordX(x));
+            TB.SetValue(Canvas.TopProperty, convertCoordY(y));
+            TB.TextWrapping = System.Windows.TextWrapping.Wrap;
+            TB.Width = 120;
+        } //Текст c содержанием точек
+        void FocusHyperbole()
+        {
+            double c = Math.Sqrt(Math.Pow(a,2)+ Math.Pow(b, 2));
 
+                    string text = "F1( " + (x + c * cosGradusElpis).ToString("F1") + "; " + (y - c * sinGradusElpis).ToString("F1") + ")";
+                    DrawPoinFocus(x + c * cosGradusElpis, y - c * sinGradusElpis);
+                    DrawText(x + c * cosGradusElpis, y - c * sinGradusElpis, text);
+                    text = "F2( " + (x - c * cosGradusElpis).ToString("F1") + "; " + (y + c * sinGradusElpis).ToString("F1") + ")";
+                    DrawPoinFocus(x - c * cosGradusElpis, y + c * sinGradusElpis);
+                    DrawText(x - c * cosGradusElpis, y + c * sinGradusElpis, text);
+
+            
+        }
+        Ellipse PointFocus;
+        void DrawPoinFocus(double x, double y)
+        {
+
+            PointFocus = new Ellipse()//задаем прочие параметры
+            {
+                Width = (maxX / count) * 0.2,
+                Height = (maxX / count) * 0.2,
+                Fill = Brushes.Black,
+                Stroke = Brushes.Black,
+                StrokeThickness = 1
+            };
+
+            cv.Children.Add(PointFocus);//помещаем на канву
+                                        //в нужную точку канвы
+            PointFocus.SetValue(Canvas.LeftProperty, convertCoordX(x - 0.1));
+            PointFocus.SetValue(Canvas.TopProperty, convertCoordY(y + 0.1));
+        }
 
         /// <summary>
         /// метод для преобразования координаты Х их канвы в декартово значение
@@ -96,6 +146,10 @@ namespace InteractivePoster.Finction
         public double convertY(double y)
         {
             return maxX / 2 + y / -1 * (maxX / count) - rectangleB;
+        }
+        public double convertRadian(double r)
+        {
+            return r / 180 * Math.PI;
         }
     }
 
